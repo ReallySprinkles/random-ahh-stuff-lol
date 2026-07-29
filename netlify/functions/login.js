c814665bb128f82f897baf00fe964714e1484149
sprinkles <ovendalegend@gmail.com>
2026-07-29 14:48:23 -0700

    Update login.js

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,6 +1,5 @@
 exports.handler = async (event, context) => {
   console.log("--> LOGIN REQUEST PATH:", event.path);
-  console.log("--> METHOD:", event.httpMethod);
 
   const headers = {
     "Content-Type": "application/json",
@@ -14,28 +13,29 @@ exports.handler = async (event, context) => {
     return { statusCode: 200, headers, body: "" };
   }
 
-  // Exact legacy Passport success payload structure
-  const responsePayload = {
-    message: "success",
-    status_code: 0,
-    data: {
-      error_code: 0,
-      is_login: 1,
-      session_key: "mock_session_active_12345",
-      user_id: "735712345678",
-      uid: "735712345678",
-      screen_name: "Sprinkles",
-      unique_id: "sprinkles.dude",
-      sec_user_id: "MS4wLjABAAAA_mock_sec_uid_12345",
-      visitor_id: "",
-      register_time: 1600000000,
-      status: 1
-    }
-  };
-
   return {
     statusCode: 200,
     headers,
-    body: JSON.stringify(responsePayload)
+    body: JSON.stringify({
+      "message": "success",
+      "status_code": 0,
+      "error_code": 0,
+      "data": {
+        "error_code": 0,
+        "description": "",
+        "user_id": "735712345678",
+        "user_id_str": "735712345678",
+        "sec_user_id": "MS4wLjABAAAA_mock_sec_uid_12345",
+        "unique_id": "sprinkles.dude",
+        "screen_name": "Sprinkles",
+        "is_login": true,
+        "session_key": "mock_session_active_12345",
+        "passport_user_id": "735712345678",
+        "email": "sprinkles@dude.com",
+        "mobile": "",
+        "visitor_id": "",
+        "status": 1
+      }
+    })
   };
 };
