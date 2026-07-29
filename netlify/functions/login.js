96f55dd6f96fbab80d47f998ff019d7ff4b6e459
sprinkles <ovendalegend@gmail.com>
2026-07-29 14:49:55 -0700

    Update login.js

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,12 +1,10 @@
 exports.handler = async (event, context) => {
-  console.log("--> LOGIN REQUEST PATH:", event.path);
-
   const headers = {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "*",
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-    "Set-Cookie": "sessionid=mock_session_active_12345; Path=/; Domain=.netlify.app; HttpOnly"
+    "Set-Cookie": "sessionid=mock_session_active_12345; Path=/; Domain=.netlify.app; HttpOnly, sid_tt=mock_session_active_12345; Path=/; Domain=.netlify.app; HttpOnly, uid_tt=735712345678; Path=/; Domain=.netlify.app; HttpOnly"
   };
 
   if (event.httpMethod === "OPTIONS") {
@@ -17,24 +15,21 @@ exports.handler = async (event, context) => {
     statusCode: 200,
     headers,
     body: JSON.stringify({
-      "message": "success",
-      "status_code": 0,
-      "error_code": 0,
-      "data": {
-        "error_code": 0,
-        "description": "",
-        "user_id": "735712345678",
-        "user_id_str": "735712345678",
-        "sec_user_id": "MS4wLjABAAAA_mock_sec_uid_12345",
-        "unique_id": "sprinkles.dude",
-        "screen_name": "Sprinkles",
-        "is_login": true,
-        "session_key": "mock_session_active_12345",
-        "passport_user_id": "735712345678",
-        "email": "sprinkles@dude.com",
-        "mobile": "",
-        "visitor_id": "",
-        "status": 1
+      message: "success",
+      status_code: 0,
+      error_code: 0,
+      data: {
+        error_code: 0,
+        description: "",
+        user_id: "735712345678",
+        user_id_str: "735712345678",
+        sec_user_id: "MS4wLjABAAAA_mock_sec_uid_12345",
+        unique_id: "sprinkles.dude",
+        screen_name: "Sprinkles",
+        is_login: true,
+        session_key: "mock_session_active_12345",
+        passport_user_id: "735712345678",
+        status: 1
       }
     })
   };
