f9e37e861fd6bee9717d4ede4bae15a1c39b4838
sprinkles <ovendalegend@gmail.com>
2026-07-30 19:12:41 +0300

    Create comment-publish.js

---
+++ netlify/edge-functions/comment-publish.js
@@ -0,0 +1,31 @@
+export default async () => {
+  return new Response(
+    JSON.stringify({
+      status_code: 0,
+      comment: {
+        cid: "9000000000000000001",
+        text: "test comment",
+        create_time: 1717000000,
+        aweme_id: "7000000000000000001",
+        user: {
+          uid: "1234567890",
+          unique_id: "le.user",
+          nickname: "le user",
+          avatar_thumb: {
+            url_list: [
+              "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/example~c5_168x168.jpeg"
+            ]
+          }
+        }
+      }
+    }),
+    {
+      status: 200,
+      headers: { "Content-Type": "application/json" }
+    }
+  );
+};
+
+export const config = {
+  path: "/aweme/v1/comment/publish/"
+};
