8c3825010e2f3f671bfbdce8ff6df3272529e3e7
sprinkles <ovendalegend@gmail.com>
2026-07-31 01:03:15 +0300

    Update youtube-profile-connection.js

--- netlify/edge-functions/youtube-profile-connection.js
+++ netlify/edge-functions/youtube-profile-connection.js
@@ -49,7 +49,7 @@ export default async (req) => {
 export const config = {
   path: [
     "/aweme/v1/user/profile/self/*",
-    "/aweme/v1/user/me/*",
+    "/aweme/v1/user/*",
     "/aweme/v1/user/detail/*",
     "/aweme/v1/social/bind/*"
   ]
