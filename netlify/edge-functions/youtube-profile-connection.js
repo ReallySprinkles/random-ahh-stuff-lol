5716328b257d2cd6e7f6cfb70ee419fe003f0069
sprinkles <ovendalegend@gmail.com>
2026-07-31 01:17:24 +0300

    Update youtube-profile-connection.js

--- netlify/edge-functions/youtube-profile-connection.js
+++ netlify/edge-functions/youtube-profile-connection.js
@@ -32,7 +32,11 @@ export default async (req) => {
       following_count: 2,
       follower_count: 2,
       total_favorited: 100000,
-      is_self: true
+      is_self: true,
+      aweme_count: 1,
+      favoriting_count: 1
+    
+    
     },
     extra: {
       now: Date.now(),
