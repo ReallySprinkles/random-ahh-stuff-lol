0f2742c6c8e3d71f978424b02b2b50eeecd791c6
sprinkles <ovendalegend@gmail.com>
2026-07-31 20:48:51 +0300

    Update user-posts.js

--- netlify/edge-functions/user-posts.js
+++ netlify/edge-functions/user-posts.js
@@ -1,8 +1,6 @@
 // netlify/edge-functions/user-feed.js
 
 export default async (req) => {
-  const url = new URL(req.url);
-
   const headers = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "Content-Type, Authorization, X-SS-REQ-TICK",
@@ -14,7 +12,45 @@ export default async (req) => {
     return new Response(null, { status: 200, headers });
   }
 
-  // --- USER POST FEED DATA ---
+  // --- RAW GITHUB PFP LINK ---
+  const myPfpUrl = "https://raw.githubusercontent.com/ReallySprinkles/random-ahh-stuff-lol/refs/heads/main/finn_the_human_pfp_.png";
+
+  // --- AUTHOR OBJECT ---
+  const authorData = {
+    uid: "7117828228",
+    short_id: "7117828228",
+    nickname: "sprinkles",
+    unique_id: "sprinkles.dude",
+    avatar_thumb: { uri: "sprinkles_avatar", url_list: [myPfpUrl] },
+    avatar_medium: { uri: "sprinkles_avatar", url_list: [myPfpUrl] },
+    avatar_168x168: { uri: "sprinkles_avatar", url_list: [myPfpUrl] },
+    avatar_300x300: { uri: "sprinkles_avatar", url_list: [myPfpUrl] },
+    custom_verify: "",
+    enterprise_verify_reason: "",
+    secret: 0,
+    is_discipline_member: false
+  };
+
+  // --- DEFAULT SOUND OBJECT ---
+  const defaultMusic = {
+    id: 7100000000000099000,
+    mid: "7100000000000099001",
+    title: "original sound - sprinkles",
+    author: "sprinkles",
+    album: "original sound",
+    duration: 15,
+    cover_thumb: { url_list: [myPfpUrl] },
+    cover_medium: { url_list: [myPfpUrl] },
+    cover_large: { url_list: [myPfpUrl] },
+    play_url: {
+      uri: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+      url_list: ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"]
+    },
+    is_original: true,
+    owner_handle: "sprinkles.dude"
+  };
+
+  // --- FEED DATA WITH PFP, SOUND, SHARE, & COMMENTS ENABLED ---
   const feedPayload = {
     status_code: 0,
     has_more: 0,
@@ -25,111 +61,153 @@ export default async (req) => {
         aweme_id: "7100000000000000001",
         desc: "55 people I love you only ahh song 🥀 #Eminem #Ronaldo",
         is_top: 1,
-        statistics: { play_count: 2100000, digg_count: 157500, comment_count: 2235, collect_count: 30600 },
-        video: {
-          cover: {
-            url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604678"]
-          }
-        }
+        author: authorData,
+        music: defaultMusic,
+        item_comment_settings: 0,
+        comment_permission_status: 0,
+        allow_comment: 1,
+        share_url: "https://vm.tiktok.com/ZM8123456/",
+        share_info: {
+          share_url: "https://vm.tiktok.com/ZM8123456/",
+          share_desc: "Check out this video!",
+          share_title: "55 people I love you only ahh song 🥀"
+        },
+        statistics: { play_count: 2100000, digg_count: 157500, comment_count: 2235, collect_count: 30600, share_count: 1240 },
+        video: { cover: { url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604678"] } }
       },
       {
         aweme_id: "7100000000000000002",
         desc: "Ain't no way 😭✌️ #fyp #roblox #epstein #epsteinfiles #epsteinroblox",
         is_top: 1,
-        statistics: { play_count: 1300000, digg_count: 63100, comment_count: 665, collect_count: 5732 },
-        video: {
-          cover: {
-            url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604679"]
-          }
-        }
+        author: authorData,
+        music: defaultMusic,
+        item_comment_settings: 0,
+        comment_permission_status: 0,
+        allow_comment: 1,
+        share_url: "https://vm.tiktok.com/ZM8123457/",
+        share_info: {
+          share_url: "https://vm.tiktok.com/ZM8123457/",
+          share_desc: "Check out this video!",
+          share_title: "Ain't no way 😭✌️"
+        },
+        statistics: { play_count: 1300000, digg_count: 63100, comment_count: 665, collect_count: 5732, share_count: 890 },
+        video: { cover: { url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604679"] } }
       },
       {
         aweme_id: "7100000000000000003",
         desc: "hi #Eminem #Ronaldo #eladeselasobinubaliepraso",
         is_top: 1,
-        statistics: { play_count: 739400, digg_count: 70700, comment_count: 1107, collect_count: 9742 },
-        video: {
-          cover: {
-            url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604680"]
-          }
-        }
+        author: authorData,
+        music: defaultMusic,
+        item_comment_settings: 0,
+        comment_permission_status: 0,
+        allow_comment: 1,
+        share_url: "https://vm.tiktok.com/ZM8123458/",
+        share_info: {
+          share_url: "https://vm.tiktok.com/ZM8123458/",
+          share_desc: "Check out this video!",
+          share_title: "hi"
+        },
+        statistics: { play_count: 739400, digg_count: 70700, comment_count: 1107, collect_count: 9742, share_count: 450 },
+        video: { cover: { url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604680"] } }
       },
       {
         aweme_id: "7100000000000000004",
         desc: "#CapCut",
         is_top: 0,
-        statistics: { play_count: 216, digg_count: 10, comment_count: 3, collect_count: 2 },
-        video: {
-          cover: {
-            url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604681"]
-          }
-        }
+        author: authorData,
+        music: defaultMusic,
+        item_comment_settings: 0,
+        comment_permission_status: 0,
+        allow_comment: 1,
+        share_url: "https://vm.tiktok.com/ZM8123459/",
+        share_info: { share_url: "https://vm.tiktok.com/ZM8123459/", share_title: "#CapCut" },
+        statistics: { play_count: 216, digg_count: 10, comment_count: 3, collect_count: 2, share_count: 0 },
+        video: { cover: { url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604681"] } }
       },
       {
         aweme_id: "7100000000000000005",
         desc: "",
         is_top: 0,
-        statistics: { play_count: 306, digg_count: 18, comment_count: 4, collect_count: 3 },
-        video: {
-          cover: {
-            url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604682"]
-          }
-        }
+        author: authorData,
+        music: defaultMusic,
+        item_comment_settings: 0,
+        comment_permission_status: 0,
+        allow_comment: 1,
+        share_url: "https://vm.tiktok.com/ZM8123460/",
+        share_info: { share_url: "https://vm.tiktok.com/ZM8123460/", share_title: "Video" },
+        statistics: { play_count: 306, digg_count: 18, comment_count: 4, collect_count: 3, share_count: 1 },
+        video: { cover: { url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604682"] } }
       },
       {
         aweme_id: "7100000000000000006",
         desc: "",
         is_top: 0,
-        statistics: { play_count: 0, digg_count: 24, comment_count: 2, collect_count: 10 },
-        video: {
-          cover: {
-            url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604683"]
-          }
-        }
+        author: authorData,
+        music: defaultMusic,
+        item_comment_settings: 0,
+        comment_permission_status: 0,
+        allow_comment: 1,
+        share_url: "https://vm.tiktok.com/ZM8123461/",
+        share_info: { share_url: "https://vm.tiktok.com/ZM8123461/", share_title: "Video" },
+        statistics: { play_count: 0, digg_count: 24, comment_count: 2, collect_count: 10, share_count: 0 },
+        video: { cover: { url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604683"] } }
       },
       {
         aweme_id: "7100000000000000007",
         desc: "",
         is_top: 0,
-        statistics: { play_count: 956, digg_count: 22, comment_count: 5, collect_count: 7 },
-        video: {
-          cover: {
-            url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604684"]
-          }
-        }
+        author: authorData,
+        music: defaultMusic,
+        item_comment_settings: 0,
+        comment_permission_status: 0,
+        allow_comment: 1,
+        share_url: "https://vm.tiktok.com/ZM8123462/",
+        share_info: { share_url: "https://vm.tiktok.com/ZM8123462/", share_title: "Video" },
+        statistics: { play_count: 956, digg_count: 22, comment_count: 5, collect_count: 7, share_count: 2 },
+        video: { cover: { url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604684"] } }
       },
       {
         aweme_id: "7100000000000000008",
         desc: "#CapCut",
         is_top: 0,
-        statistics: { play_count: 1022, digg_count: 32, comment_count: 7, collect_count: 7 },
-        video: {
-          cover: {
-            url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604685"]
-          }
-        }
+        author: authorData,
+        music: defaultMusic,
+        item_comment_settings: 0,
+        comment_permission_status: 0,
+        allow_comment: 1,
+        share_url: "https://vm.tiktok.com/ZM8123463/",
+        share_info: { share_url: "https://vm.tiktok.com/ZM8123463/", share_title: "#CapCut" },
+        statistics: { play_count: 1022, digg_count: 32, comment_count: 7, collect_count: 7, share_count: 3 },
+        video: { cover: { url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604685"] } }
       },
       {
         aweme_id: "7100000000000000009",
         desc: "app is called L.L.E",
         is_top: 0,
-        statistics: { play_count: 350, digg_count: 8, comment_count: 6, collect_count: 2 },
-        video: {
-          cover: {
-            url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604686"]
-          }
-        }
+        author: authorData,
+        music: defaultMusic,
+        item_comment_settings: 0,
+        comment_permission_status: 0,
+        allow_comment: 1,
+        share_url: "https://vm.tiktok.com/ZM8123464/",
+        share_info: { share_url: "https://vm.tiktok.com/ZM8123464/", share_title: "app is called L.L.E" },
+        statistics: { play_count: 350, digg_count: 8, comment_count: 6, collect_count: 2, share_count: 1 },
+        video: { cover: { url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604686"] } }
       },
       {
         aweme_id: "7100000000000000010",
         desc: "#CapCut",
         is_top: 0,
-        statistics: { play_count: 571, digg_count: 10, comment_count: 3, collect_count: 4 },
-        video: {
-          cover: {
-            url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604687"]
-          }
-        }
+        author: authorData,
+        music: defaultMusic,
+        item_comment_settings: 0,
+        comment_permission_status: 0,
+        allow_comment: 1,
+        share_url: "https://vm.tiktok.com/ZM8123465/",
+        share_info: { share_url: "https://vm.tiktok.com/ZM8123465/", share_title: "#CapCut" },
+        statistics: { play_count: 571, digg_count: 10, comment_count: 3, collect_count: 4, share_count: 0 },
+        video: { cover: { url_list: ["https://p16-va-tiktok.ibyteimg.com/obj/musically-maliva-obj/1654573752604687"] } }
       }
     ]
   };
