c8f6586f15d16e1b370bc17d96a2416ff9ae4d91
sprinkles <ovendalegend@gmail.com>
2026-08-05 12:52:13 +0300

    Update discover-banner.js

--- netlify/edge-functions/discover-banner.js
+++ netlify/edge-functions/discover-banner.js
@@ -20,7 +20,7 @@ export default async (req) => {
     banner: [
       {
         bid: "1001",
-        title: "hi",
+        title: "sprinkles",
         banner_url: {
           uri: "banner/1001.png",
           url_list: [bannerImageUrl]
