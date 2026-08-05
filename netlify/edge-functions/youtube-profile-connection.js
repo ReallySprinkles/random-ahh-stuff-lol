faa29e047ced3f62bb6044c631ec31e11e7bae03
sprinkles <ovendalegend@gmail.com>
2026-08-05 17:31:25 +0300

    Update youtube-profile-connection.js

--- netlify/edge-functions/youtube-profile-connection.js
+++ netlify/edge-functions/youtube-profile-connection.js
@@ -14,42 +14,29 @@ export default async (req) => {
     return new Response(null, { status: 200, headers });
   }
 
-  const profileUrl = `https://raw.githubusercontent.com/ReallySprinkles/random-ahh-stuff-lol/refs/heads/main/finn_the_human_pfp_.png`;
-  const qrImageUrl = `https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${encodeURIComponent(profileUrl)}`;
-  const myPfpUrl = "https://raw.githubusercontent.com/ReallySprinkles/random-ahh-stuff-lol/refs/heads/main/IMG_3824.jpeg";
+  // --- HELPER TO FORMAT AVATARS FOR OLDER APP BUILDS ---
+  const makeAvatarObj = (imgUrl) => ({
+    uri: imgUrl,
+    url_list: [imgUrl, imgUrl],
+    width: 720,
+    height: 720
+  });
 
-  // --- AUTHOR MAP MATCHING NEW UIDs IN FEED-HANDLER ---
+  const pfpFinn = "https://raw.githubusercontent.com/ReallySprinkles/random-ahh-stuff-lol/refs/heads/main/finn_the_human_pfp_.png";
+  const pfpDefault = "https://raw.githubusercontent.com/ReallySprinkles/random-ahh-stuff-lol/refs/heads/main/IMG_3824.jpeg";
+  const pfpChucky = "https://raw.githubusercontent.com/ReallySprinkles/random-ahh-stuff-lol/refs/heads/main/IMG_4238.jpeg";
+
+  // --- REGISTRY FOR ALL CREATORS (Including sprinkles.dude) ---
   const authorRegistry = {
-    "7000000001": { nickname: "bloxyz wiedlak kasefar", unique_id: "bloxyz.wiedlak.kasefar", avatar: "https://raw.githubusercontent.com/ReallySprinkles/random-ahh-stuff-lol/refs/heads/main/IMG_3824.jpeg" },
-    "7000000002": { nickname: "Gojo lover", unique_id: "gojo.lover676", avatar: "https://raw.githubusercontent.com/ReallySprinkles/random-ahh-stuff-lol/refs/heads/main/IMG_3824.jpeg" },
-    "7000000003": { nickname: "💲", unique_id: "unknownscarface1", avatar: "https://raw.githubusercontent.com/ReallySprinkles/random-ahh-stuff-lol/refs/heads/main/IMG_3824.jpeg" },
-    "7000000004": { nickname: "𝘿𝙮𝙡𝙖𝙣𝙖𝙣𝙏𝙚𝙘𝙝", unique_id: "dylbanantech", avatar: "https://raw.githubusercontent.com/ReallySprinkles/random-ahh-stuff-lol/refs/heads/main/IMG_3824.jpeg" },
-    "7000000005": { nickname: "drtenmalonglost3rdson", unique_id: "drtenmalonglost3rdson", avatar: "https://raw.githubusercontent.com/ReallySprinkles/random-ahh-stuff-lol/refs/heads/main/IMG_4238.jpeg" },
-    "7000000006": { nickname: "sprinkles", unique_id: "sprinkles.dude", avatar: "https://raw.githubusercontent.com/ReallySprinkles/random-ahh-stuff-lol/refs/heads/main/finn_the_human_pfp_.png" }
+    "7000000001": { nickname: "bloxyz wiedlak kasefar", unique_id: "bloxyz.wiedlak.kasefar", avatar: pfpDefault },
+    "7000000002": { nickname: "Gojo lover", unique_id: "gojo.lover676", avatar: pfpDefault },
+    "7000000003": { nickname: "💲", unique_id: "unknownscarface1", avatar: pfpDefault },
+    "7000000004": { nickname: "𝘿𝙮𝙡𝙖𝙣𝙖𝙣𝙏𝙚𝙘𝙝", unique_id: "dylbanantech", avatar: pfpDefault },
+    "7000000005": { nickname: "drtenmalonglost3rdson", unique_id: "drtenmalonglost3rdson", avatar: pfpChucky },
+    "7000000006": { nickname: "sprinkles", unique_id: "sprinkles.dude", avatar: pfpDefault }
   };
 
-  const sharedUserList = [
-    { uid: "7000000001", nickname: "motorowiec", unique_id: "motorowiec10", follow_status: 0 },
-    { uid: "7000000002", nickname: "Milo Korfalı,,", unique_id: "orphanminsu", follow_status: 0 },
-    { uid: "7000000003", nickname: "Ate Nang", unique_id: "ate.nang", follow_status: 0 },
-    { uid: "7000000004", nickname: "weluvelvinn", unique_id: "weluvelvinn", follow_status: 0 },
-    { uid: "7000000005", nickname: "drtenmalonglost3rdson", unique_id: "drtenmalonglost3rdson", follow_status: 0 },
-    { uid: "7000000006", nickname: "sprinkles", unique_id: "sprinkles.dude", follow_status: 0 }
-  ];
-
-  if (url.pathname.includes("/follower/list")) {
-    return new Response(JSON.stringify({ status_code: 0, followers: sharedUserList, total: 4525, has_more: 0 }), { status: 200, headers });
-  }
-
-  if (url.pathname.includes("/following/list")) {
-    return new Response(JSON.stringify({ status_code: 0, followings: sharedUserList, total: 1188, has_more: 0 }), { status: 200, headers });
-  }
-
-  if (url.pathname.includes("/qrcode")) {
-    return new Response(JSON.stringify({ status_code: 0, status_msg: "", qrcode_url: { uri: "qrcode/7000000006.png", url_list: [qrImageUrl] } }), { status: 200, headers });
-  }
-
-  // --- EXTRACT USER ID ---
+  // --- EXTRACT TARGET USER ID ---
   let targetUid = url.searchParams.get("user_id") || url.searchParams.get("sec_user_id");
 
   const pathParts = url.pathname.split("/").filter(Boolean);
@@ -69,46 +56,28 @@ export default async (req) => {
     }
   }
 
-  // --- CHECK SELF VS OTHER ---
-  const isSelfProfile = url.pathname.includes("/profile/self") || targetUid === "7000000006" || targetUid === "7117828228";
-  const isOtherProfile = url.pathname.includes("/profile/other") || (targetUid && !isSelfProfile);
-
-  if (isOtherProfile && !isSelfProfile) {
-    const authorData = authorRegistry[targetUid] || {
-      nickname: "drtenmalonglost3rdson",
-      unique_id: "drtenmalonglost3rdson",
-      avatar: "https://raw.githubusercontent.com/ReallySprinkles/random-ahh-stuff-lol/refs/heads/main/IMG_4238.jpeg"
-    };
+  // --- DYNAMIC AUTHOR LOOKUP (OTHER PROFILE) ---
+  if (url.pathname.includes("/profile/other") || (targetUid && targetUid !== "7117828228")) {
+    const authorData = authorRegistry[targetUid] || authorRegistry["7000000006"];
+    const avatarData = makeAvatarObj(authorData.avatar);
 
     return new Response(
       JSON.stringify({
         status_code: 0,
         user: {
-          uid: targetUid || "7000000005",
-          short_id: targetUid || "7000000005",
+          uid: targetUid || "7000000006",
+          short_id: targetUid || "7000000006",
           nickname: authorData.nickname,
           unique_id: authorData.unique_id,
           signature: "",
           secret: 0,
           is_private: false,
-          allow_others_to_find_me: 1,
 
-          comment_setting: 0,
-          duet_setting: 0,
-          react_setting: 0,
-          stitch_setting: 0,
-          download_setting: 0,
-
-          follow_status: 0,
-          follower_status: 0,
-          is_following: false,
-          is_followed: false,
-
-          avatar_thumb: { uri: "avatar.jpeg", url_list: [authorData.avatar] },
-          avatar_medium: { uri: "avatar.jpeg", url_list: [authorData.avatar] },
-          avatar_larger: { uri: "avatar.jpeg", url_list: [authorData.avatar] },
-          avatar_168x168: { uri: "avatar.jpeg", url_list: [authorData.avatar] },
-          avatar_300x300: { uri: "avatar.jpeg", url_list: [authorData.avatar] },
+          avatar_thumb: avatarData,
+          avatar_medium: avatarData,
+          avatar_larger: avatarData,
+          avatar_168x168: avatarData,
+          avatar_300x300: avatarData,
 
           following_count: 0,
           follower_count: 0,
@@ -122,56 +91,26 @@ export default async (req) => {
     );
   }
 
-  // --- MY MAIN PROFILE PAYLOAD ---
+  // --- SELF PROFILE (Your logged-in account, isolated from sprinkles.dude) ---
+  const myAvatar = makeAvatarObj(pfpFinn);
+
   return new Response(
     JSON.stringify({
       status_code: 0,
       user: {
-        uid: "7000000006",
-        short_id: "7000000006",
+        uid: "7117828228",
+        short_id: "7117828228",
         nickname: "sprinkles",
         unique_id: "sprinkles.dude",
         signature: "I'm really sprinkles 🤯\nEgyptian 🇪🇬\n(NOT A TECHTOKER!)\nDiscord username: reallysprinkles\nDISCORD SERVER HERE 👇\nhttps://discord.gg/Ta8ZtP4sCf",
         secret: 0,
         is_private: false,
-        allow_others_to_find_me: 1,
 
-        comment_setting: 0,
-        duet_setting: 0,
-        react_setting: 0,
-        stitch_setting: 0,
-        download_setting: 0,
-
-        follow_status: 2,
-        follower_status: 2,
-        is_following: true,
-        is_followed: true,
-
-        gender: 2,
-        birthday: "2010-04-07",
-        city: "Cairo",
-        country: "EG",
-        location: "Cairo, Egypt",
-        ip_location: "Egypt",
-
-        telephone: "+15550192831",
-        phone: "+15550192831",
-        mobile: "+15550192831",
-        bind_phone: "+15550192831",
-        is_phone_bound: true,
-        has_password: true,
-        has_email: true,
-
-        avatar_thumb: { uri: "sprinkles_avatar.jpeg", url_list: [myPfpUrl] },
-        avatar_medium: { uri: "sprinkles_avatar.jpeg", url_list: [myPfpUrl] },
-        avatar_larger: { uri: "sprinkles_avatar.jpeg", url_list: [myPfpUrl] },
-        avatar_168x168: { uri: "sprinkles_avatar.jpeg", url_list: [myPfpUrl] },
-        avatar_300x300: { uri: "sprinkles_avatar.jpeg", url_list: [myPfpUrl] },
-
-        qrcode_url: { uri: "qrcode/7000000006.png", url_list: [qrImageUrl] },
-        youtube_channel_id: "UCC45pszowTR4u8OrY0HBYPA",
-        youtube_channel_title: "sprinkles",
-        ins_id: "iamreallysprinkles",
+        avatar_thumb: myAvatar,
+        avatar_medium: myAvatar,
+        avatar_larger: myAvatar,
+        avatar_168x168: myAvatar,
+        avatar_300x300: myAvatar,
 
         following_count: 1188,
         follower_count: 4525,
@@ -192,13 +131,6 @@ export const config = {
     "/aweme/v1/user/profile/other",
     "/aweme/v1/user",
     "/aweme/v1/user/*",
-    "/aweme/v1/user/detail/*",
-    "/aweme/v1/user/follower/list/*",
-    "/aweme/v1/user/follower/list",
-    "/aweme/v1/user/following/list/*",
-    "/aweme/v1/user/following/list",
-    "/aweme/v1/social/bind/*",
-    "/aweme/v1/qrcode/*",
-    "/aweme/v1/user/qrcode/*"
+    "/aweme/v1/user/detail/*"
   ]
 };
