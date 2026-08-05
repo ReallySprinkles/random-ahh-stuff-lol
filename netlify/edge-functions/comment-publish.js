bdd9108846839f66a385390de5c0a90e7587cf07
sprinkles <ovendalegend@gmail.com>
2026-08-05 20:53:08 +0300

    Update comment-publish.js

--- netlify/edge-functions/comment-publish.js
+++ netlify/edge-functions/comment-publish.js
@@ -21,6 +21,14 @@ export default async (req) => {
     console.error("Failed to parse incoming comment body:", err);
   }
 
+  const defaultPic = "https://raw.githubusercontent.com/ReallySprinkles/random-ahh-stuff-lol/refs/heads/main/finn_the_human_pfp_.png";
+  const avatarObj = {
+    uri: defaultPic,
+    url_list: [defaultPic, defaultPic],
+    width: 720,
+    height: 1280
+  };
+
   return new Response(
     JSON.stringify({
       status_code: 0,
@@ -28,23 +36,34 @@ export default async (req) => {
       comment: {
         cid: String(Date.now()), // Unique comment ID based on timestamp
         text: userText,          // Dynamic user input text
-        create_time: Math.floor(Date.now() / 1000),
+        create_time: Math.floor(Date.now() / 1000), // 10-digit UNIX timestamp integer
         aweme_id: "7000000000000000001",
+        digg_count: 0,           // Explicit integer for right-hand like section
+        user_digged: 0,          // Explicit flag for client interaction state
+        status: 1,               // Active comment status code
+        reply_comment: null,
         user: {
           uid: "7117828228",
+          short_id: "7117828228",
           unique_id: "sprinkles.dude",
+          display_id: "sprinkles.dude", // Required by legacy musical.ly for handle formatting
           nickname: "sprinkles",
-          avatar_thumb: {
-            url_list: [
-              "https://raw.githubusercontent.com/ReallySprinkles/random-ahh-stuff-lol/refs/heads/main/finn_the_human_pfp_.png"
-            ]
-          }
+          custom_verify: "",
+          enterprise_verify_reason: "",
+          avatar_thumb: avatarObj,
+          avatar_medium: avatarObj,
+          avatar_larger: avatarObj
         }
       }
     }),
     {
       status: 200,
-      headers: { "Content-Type": "application/json; charset=utf-8" }
+      headers: { 
+        "Content-Type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
+      }
     }
   );
 };
@@ -52,6 +71,8 @@ export default async (req) => {
 export const config = {
   path: [
     "/aweme/v1/comment/publish/",
-    "/aweme/v1/comment/publish"
+    "/aweme/v1/comment/publish",
+    "/aweme/v2/comment/publish/",
+    "/aweme/v2/comment/publish"
   ]
 };
