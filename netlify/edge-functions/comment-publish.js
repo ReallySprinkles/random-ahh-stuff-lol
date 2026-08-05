4911016023f15a33a8d16cc32e41898a429e5fb5
sprinkles <ovendalegend@gmail.com>
2026-08-05 08:36:41 +0300

    Update comment-publish.js

--- netlify/edge-functions/comment-publish.js
+++ netlify/edge-functions/comment-publish.js
@@ -4,13 +4,13 @@ export default async () => {
       status_code: 0,
       comment: {
         cid: "9000000000000000001",
-        text: "test comment",
-        create_time: 1717000000,
+        text: "no",
+        create_time: 0,
         aweme_id: "7000000000000000001",
         user: {
-          uid: "1234567890",
-          unique_id: "le.user",
-          nickname: "le user",
+          uid: "7117828228",
+          unique_id: "sprinkles.dude",
+          nickname: "sprinkles",
           avatar_thumb: {
             url_list: [
               "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/example~c5_168x168.jpeg"
